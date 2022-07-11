436af364c3226fb2c27aa85ee5379053ba45c623
Jay Sprout <jotasprout@gmail.com>
2022-07-11 17:25:41 -0400

    updated getAllUsers

--- Sprout-Autumn-P1c/Program.cs
+++ Sprout-Autumn-P1c/Program.cs
@@ -1,18 +1,23 @@
-﻿using UI;
+﻿//using UI;
+using DataAccess;
 
-MainMenu intro = new MainMenu();
-intro.Begin();
+//MainMenu intro = new MainMenu();
+//intro.Begin();
 
 // Console.WriteLine("Here is a list of all users:");
-// UserDAO peeps = new UserDAO();
+// UserRepository peeps = new UserRepository();
 // peeps.GetAllUsers();
 
 
-// UserDAO peepIWant = new UserDAO();
+// UserRepository peepIWant = new UserRepository();
 // Console.WriteLine("Which user do you want?");
 // string userIWant = Console.ReadLine();
 // peepIWant.GetUserByUserName(userIWant);
 
-// Console.WriteLine("Here is a list of all users:");
-// UserDAO peeps = new UserDAO();
-// peeps.GetAllUsers();   
+Console.WriteLine("Here is a list of all users:");
+UserRepository peeps = new UserRepository();
+peeps.GetAllUsers();
+
+Console.WriteLine("Here is a list of all Tickets:");
+TicketRepository tickets = new TicketRepository();
+tickets.GetAllTickets();
