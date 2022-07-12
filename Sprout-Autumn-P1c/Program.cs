ddb6eede342011746dc53ee13bd20bd5b8983547
Jay Sprout <jotasprout@gmail.com>
2022-07-11 20:59:06 -0400

    testing menus after user logs in

--- Sprout-Autumn-P1c/Program.cs
+++ Sprout-Autumn-P1c/Program.cs
@@ -1,18 +1,19 @@
-﻿//using UI;
-using DataAccess;
+﻿using UI;
+// using DataAccess;
 
-//MainMenu intro = new MainMenu();
-//intro.Begin();
+MainMenu intro = new MainMenu();
+intro.Begin();
 
-UserRepository peepIWant = new UserRepository();
-Console.WriteLine("Which user do you want?");
-string userIWant = Console.ReadLine();
-peepIWant.GetUserByUserName(userIWant);
+// UserRepository peepIWant = new UserRepository();
+// Console.WriteLine("Which user do you want?");
+// string userIWant = Console.ReadLine();
+// peepIWant.GetUserByUserName(userIWant);
 
-Console.WriteLine("Here is a list of all users:");
-UserRepository peeps = new UserRepository();
-peeps.GetAllUsers();
+// Console.WriteLine("Here is a list of all users:");
+// UserRepository peeps = new UserRepository();
+// peeps.GetAllUsers();
 
-Console.WriteLine("Here is a list of all Tickets:");
-TicketRepository tickets = new TicketRepository();
-tickets.GetAllTickets();
+// Console.WriteLine("Here is a list of all Tickets:");
+// TicketRepository tickets = new TicketRepository();
+// tickets.GetAllTickets();
+
